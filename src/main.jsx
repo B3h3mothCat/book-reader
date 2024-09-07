2b2c648f1e327315138da15570476ffd81b3c190
B3h3mothCat <pavromsys@ya.ru>
2024-09-07 22:07:34 +0300

    - Redux toolkit added

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
+import { Provider } from "react-redux"
+import store from "./store.js"
 
 import { BrowserRouter } from 'react-router-dom'
 
@@ -11,9 +12,13 @@ import { BrowserRouter } from 'react-router-dom'
 const root = document.getElementById('root')
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+
+
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 )
